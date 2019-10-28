e636f8c7da0524258a57d1cca25e4138edcd46c1
JD <diazjose195@gmail.com>
2019-10-28 11:56:54 -0500

    Remaster

---
+++ Biometrico/CLocation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Device.Location;
+
+namespace BiometriaTawaCSharp
+{
+    class CLocation
+    {
+        public static string GetLocationProperty()
+        {
+            string cordenadas = "";
+            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
+            // Do not suppress prompt, and wait 1000 milliseconds to start.
+            watcher.TryStart(false, TimeSpan.FromMilliseconds(1000));
+
+            GeoCoordinate coord = watcher.Position.Location;
+
+            if (coord.IsUnknown != true)
+            {
+                cordenadas="Latitud: {0}, Longitud: {1}"+coord.Latitude+coord.Longitude;
+            }
+            else
+            {
+                cordenadas="N/A";
+            }
+            return cordenadas;
+        }
+    }
+}
