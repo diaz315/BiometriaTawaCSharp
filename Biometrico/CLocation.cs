f4a886aac1a56481bd9520ea18bc57bbd7ef1abb
JD <diazjose195@gmail.com>
2019-10-31 14:07:39 -0500

    Fix_localizacion

--- Biometrico/CLocation.cs
+++ Biometrico/CLocation.cs
@@ -5,24 +5,23 @@ namespace BiometriaTawaCSharp
 {
     class CLocation
     {
+        private static double latitud;
+        private static double longitud;
         public static string GetLocationProperty()
         {
             string cordenadas = "";
             GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
-            // Do not suppress prompt, and wait 1000 milliseconds to start.
-            watcher.TryStart(false, TimeSpan.FromMilliseconds(1000));
+            watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+            watcher.Start(true);
 
-            GeoCoordinate coord = watcher.Position.Location;
+            cordenadas = latitud + "-" + longitud;
 
-            if (coord.IsUnknown != true)
-            {
-                cordenadas="Latitud: {0}, Longitud: {1}"+coord.Latitude+coord.Longitude;
-            }
-            else
-            {
-                cordenadas="N/A";
-            }
             return cordenadas;
+        }
+        private static void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            latitud = e.Position.Location.Latitude;
+            longitud = e.Position.Location.Longitude;
         }
     }
 }
